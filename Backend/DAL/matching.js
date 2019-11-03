095e408846528f31cc128f01f2cf0010ef05ad16
Tejaswini <tramatejaswini@gmail.com>
2019-11-03 11:38:42 -0800

    Update matching.js

--- Backend/DAL/matching.js
+++ Backend/DAL/matching.js
@@ -1,8 +1,9 @@
 const _tableName1 = 'Student';
 const _tableName2 = 'Interviewer';
 
-const getMatching = connection => (person = { Student_ID : req.body}) => {
-    const {Student_ID, Interviewer_ID } = person;
+const getMatching = connection => (person = { }) => {
+    const Student_ID = 2; 
+    const {Interviewer_ID } = person;
     //let query = `select * from ${_tableName1} INNER JOIN ${_tableName2} ON ${_tableName1}.Career_Interests = ${_tableName2}.Career_Fields where ${_tableName2}.Available = 1 LIMIT 2`;
     let query = `select * from Interviewer_Skills as sk , Interviewer as i , Student as s where Interviewer_Skill in (SELECT Student_Skill from Student_Skills where Student_Id = '${Student_ID}') and sk.Interviewer_Id=i.Interviewer_ID and i.Available = 1`;
     const clause = [];
@@ -51,9 +52,28 @@ const saveInterviewDetails = connection => interviewDetails => {
     });
 };
 
+const updateInterviewerStatus = connection => interviewDetailsUpdate => {
+    const { Interviewer_Id } = interviewDetailsUpdate;
+    let query = `UPDATE Interviewer SET Available = 0 WHERE Interviewer_Id = ${Interviewer_Id};`;
+    
+    return new Promise((resolve, reject) => {
+        connection.query(query, (error, results, fields) => {
+            // release connection first!
+            connection.release();
+
+            if (error) {
+                reject(error);
+            } else {
+                resolve({ results, fields });
+            }
+        });
+    });
+};
+
 module.exports = {
     getMatching,
-    saveInterviewDetails
+    saveInterviewDetails,
+    updateInterviewerStatus
     
 
 };
